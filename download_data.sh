0242f5f22423ed621fa451bfad99fa78808a45f2
Shogo Ujiie <suzzz428@gmail.com>
2020-07-28 19:14:56 +0900

    fix download script

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,8 @@ if [ -d data ]; then
     echo "path ./data is already exists"
 else
     mkdir data
-    wget http://aoi.naist.jp/MedEXJ2/pretrained -O data/pretrained
+    mkdir data/pretrained
+    wget http://aoi.naist.jp/MedEXJ2/pretrained/final.model -O data/pretrained/final.model
+    wget http://aoi.naist.jp/MedEXJ2/pretrained/labels.txt -O data/pretrained/labels.txt
     wget http://aoi.naist.jp/MedEXJ2/norm_dic.csv -O data/norm_dic.csv
 fi
